91693c6f8bac33222ad1fdcf4d4c8ee34f93983a
Victory Oseiwe <thisisvictoryoseiwe@gmail.com>
2025-03-24 07:00:48 +0100

    Navbar completed. Now mobile responsive

--- src/component/Icons.jsx
+++ src/component/Icons.jsx
@@ -1,5 +1,7 @@
 import { FiArrowUpRight } from "react-icons/fi";
 import { RiMenuFill } from "react-icons/ri";
+import { IoIosClose } from "react-icons/io";
 
 export const UpRightArrow = FiArrowUpRight;
 export const Menu = RiMenuFill;
+export const CloseMenu = IoIosClose;
--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -3,14 +3,21 @@ import "./Navbar.css";
 import { NavLink, useNavigate } from "react-router-dom";
 import { UpRightArrow } from "../Icons";
 import ListItem from "../ListItem";
+import { Menu, CloseMenu } from "../Icons";
+import { useState } from "react";
 
 export default function Navbar() {
   const navigate = useNavigate();
+  const [sideBar, setSideBar] = useState(false);
 
   const signUP = () => {
     //Handle navigation to sign page here
     navigate("/signup");
     alert("Sign up button clicked");
+  };
+
+  const showSideBar = () => {
+    setSideBar(!sideBar);
   };
 
   return (
@@ -19,22 +26,58 @@ export default function Navbar() {
         <NavLink to={"/"}>
           <h1 className="logo">m2m</h1>
         </NavLink>
-        <ul className="navbar-list">
-          <ListItem>
-            <NavLink to={"/Home"}>Home</NavLink>
-          </ListItem>
-          <ListItem>
-            <NavLink to={"/about"}>About</NavLink>
-          </ListItem>
-          <ListItem>
-            <NavLink to={"/contact"}>Contact</NavLink>
-          </ListItem>
-        </ul>
+        <div className="navbar-list">
+          <NavLink className={"nav-link"} to={"/"}>
+            <ListItem>Home</ListItem>
+          </NavLink>
+          <NavLink className={"nav-link"} to={"/about"}>
+            <ListItem>About</ListItem>
+          </NavLink>
+          <NavLink className={"nav-link"} to={"/contact"}>
+            <ListItem>Contact</ListItem>
+          </NavLink>
+        </div>
         <Button className={"navbar-button"} onClick={signUP}>
           Try it
           <UpRightArrow size={25} />
         </Button>
+        <div className="mobile-menu-icon" onClick={showSideBar}>
+          <Menu size={25} />
+        </div>
       </nav>
+      {/* mobile menu  */}
+      <div className={`sidebar ${sideBar ? "show" : ""} `}>
+        <div className="mobile-menu-close" onClick={showSideBar}>
+          <CloseMenu size={25} />
+        </div>
+        <div className="mobile-menu-content">
+          <div className="mobile-menu-link">
+            <NavLink className={"link"} to={"/Home"}>
+              <ListItem>Home</ListItem>
+            </NavLink>
+            <NavLink className={"link"} to={"/about"}>
+              <ListItem>About</ListItem>
+            </NavLink>
+            <NavLink className={"link"} to={"/contact"}>
+              <ListItem>Contact</ListItem>
+            </NavLink>
+          </div>
+          <Button className={"mobile-menu-button"} onClick={signUP}>
+            Try it
+            <UpRightArrow size={25} />
+          </Button>
+          <p
+            style={{
+              fontSize: "var(--xsmall)",
+              color: "var(--yellow)",
+              textAlign: "center",
+            }}
+          >
+            Courtesy @victoryoseiwe
+          </p>
+        </div>
+      </div>
+      {sideBar && <div className="overlay" onClick={showSideBar}></div>}
     </>
   );
 }
