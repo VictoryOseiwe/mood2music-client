c48526d255d17fbdd3a5e3334c2d8d0d975817ab
Victory Oseiwe <thisisvictoryoseiwe@gmail.com>
2025-03-27 19:09:18 +0100

    Auth pages working but not fully functional and ofcourse it's not mobile responsive YET

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,29 @@ import SignIn from "./UI/Auth/SignIn";
 import NotFoundPage from "./UI/notfoundpage/Notfound";
 import Dashboard from "./UI/dashboard/Dashboard";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./utils/AuthContext";
+import ProtectedRoute from "./utils/ProtectedRoute";
 
 export default function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/signin" element={<SignIn />} />
-          {/* authenticated route */}
-          <Route path="/dashboard" element={<Dashboard />} />{" "}
-          <Route path="*" element={<NotFoundPage />} /> {/* catch-all route */}
-        </Routes>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/signin" element={<SignIn />} />
+            {/* authenticated route */}
+            <Route element={<ProtectedRoute />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+            </Route>
+            <Route path="*" element={<NotFoundPage />} />
+            {/* catch-all route */}
+          </Routes>
+        </Router>
+      </AuthProvider>
     </>
   );
 }
---
+++ src/utils/AuthContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const login = (userData) => {
+    setUser(userData);
+    localStorage.setItem("user", JSON.stringify(userData));
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+  };
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    } else {
+      setUser(null);
+    }
+    setLoading(false);
+  }, []);
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
