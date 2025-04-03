5e2a1c89a8f0c869a5aeb656c1463d6042e2c4f5
Victory Oseiwe <thisisvictoryoseiwe@gmail.com>
2025-04-03 21:36:23 +0100

    Styling music card

--- src/component/musicCard/MusicCard.jsx
+++ src/component/musicCard/MusicCard.jsx
@@ -1,8 +1,9 @@
 import Button from "../Button";
+import "./musicCard.css";
 
 export default function musicCard({ playButton, playlistImg, playButton }) {
   return (
-    <div>
+    <div className="music-card-container">
       <h4>{playlistName}</h4>
       <img src={playlistImg} alt="Playlist Image" />
       <Button>{playButton}</Button>
