6a2505a45326e499dd462e0a177b5625a1a94f9a
Victory Oseiwe <thisisvictoryoseiwe@gmail.com>
2025-04-04 19:23:38 +0100

    Started using the music card component

--- src/component/musicCard/MusicCard.jsx
+++ src/component/musicCard/MusicCard.jsx
@@ -1,12 +1,16 @@
 import Button from "../Button";
 import "./musicCard.css";
 
-export default function musicCard({ playButton, playlistImg, playButton }) {
+export default function MusicCard({ playButton, playlistImg, playlistName }) {
   return (
     <div className="music-card-container">
-      <h4>{playlistName}</h4>
-      <img className="playlist-img" src={playlistImg} alt="Playlist Image" />
-      <Button className={"play-music-btn"}>{playButton}</Button>
+      <div>
+        <img className="playlist-img" src={playlistImg} alt="Playlist Image" />
+      </div>
+      <div>
+        <h4>{playlistName}</h4>
+        <Button className={"play-music-btn"}>{playButton}</Button>
+      </div>
     </div>
   );
 }
