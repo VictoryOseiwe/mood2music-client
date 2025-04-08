91580ac9f1f76952744fcb56bc4047aa4370c1dc
Victory Oseiwe <thisisvictoryoseiwe@gmail.com>
2025-04-08 05:16:22 +0100

    Adding playlist card

--- src/component/card/PlaylistCard.jsx
+++ src/component/card/PlaylistCard.jsx
@@ -1,8 +1,34 @@
+import { useEffect, useState } from "react";
 import "./Card.css";
-export default function Card({}) {
+import axios from "axios";
+export default function PlaylistCard() {
+  const [playlists, setPlaylists] = useState();
+
+  useEffect(() => {
+    const fetchPlaylists = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:3000/playlist/getplaylist",
+          {
+            withCredentials: true,
+          }
+        );
+        setPlaylists(response.data.allPlaylists);
+      } catch (error) {}
+    };
+    fetchPlaylists();
+  }, []);
   return (
     <div>
       <h1>Card for both music and playlist</h1>
+      {playlists &&
+        playlists.map((playlist) => (
+          <div className="card" key={playlist._id}>
+            <img src={playlist.image} alt={playlist.name} />
+            <h2>{playlist.name}</h2>
+            <p>{playlist.description}</p>
+          </div>
+        ))}
     </div>
   );
 }
