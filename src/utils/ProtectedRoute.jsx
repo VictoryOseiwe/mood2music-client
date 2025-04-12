c137ba76258585e90e1d0237cd58f1c0eca61d5a
Victory Oseiwe <thisisvictoryoseiwe@gmail.com>
2025-04-12 06:01:05 +0100

    Adding spinner to prevent flickering

--- src/utils/ProtectedRoute.jsx
+++ src/utils/ProtectedRoute.jsx
@@ -1,10 +1,11 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../utils/AuthContext";
+import { Spinner } from "../component/Spinner/Spinner";
 
 const ProtectedRoute = () => {
   const { user, loading } = useAuth();
 
-  if (loading) return <p>Loading...</p>; // Prevents flickering
+  if (loading) return <Spinner />; // Prevents flickering
 
   return user ? <Outlet /> : <Navigate to="/signin" />;
 };
